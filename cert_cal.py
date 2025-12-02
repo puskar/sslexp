a03b20aa212a4d79814bb8836044bb021f2bdb5d
Michael Puskar <puskar@puskar.net>
2025-12-02 17:40:46 -0500

    Add a service to gereate a cert expiration calendar

---
+++ cert_cal.py
@@ -0,0 +1,51 @@
+import sslexp
+import icalendar
+from datetime import timedelta
+from flask import Flask, make_response
+
+
+domains = ["api.puskar.net"]
+expiry_data = {}
+for domain in domains:
+    expiry_info = sslexp.get_tls_expiration_date(domain)
+    expiry_data[domain] = expiry_info
+
+def add_alarm(days_before):
+    alarm = icalendar.Alarm()
+    alarm.add('action', 'DISPLAY')
+    alarm.add('description', f'{domain} expiry reminder')
+    alarm.add('trigger', timedelta(days =-days_before))
+    return alarm
+    
+
+def create_certcal(expiry_data):
+    cert_cal = icalendar.Calendar()
+    cert_cal.add('prodid', '-//Puskar SSLEXP//')
+    cert_cal.add('version', '2.0')
+    for domain, expiry_date in expiry_data.items():
+        event = icalendar.Event()
+        event.add('summary', f'{domain} cert expires')
+        event.add('dtstart', expiry_date)
+        event.add('dtend', expiry_date)
+        event.add('description', f'{domain} expires')
+        event.add('dtstamp', expiry_date)
+        event.add('uid', f'{domain}-{expiry_date.timestamp()}')
+        event.add_component(add_alarm(7))
+        cert_cal.add_component(event)
+    
+    return cert_cal
+
+flask_app = Flask("certcal")
+@flask_app.route('/certcal/',methods=['GET'])
+
+def main():
+    
+    cert_cal = create_certcal(expiry_data)
+    response = make_response(cert_cal.to_ical().decode('utf-8'))
+    response.headers['Content-Type'] = 'text/calendar; charset=utf-8'
+    response.headers['Content-Disposition'] = 'inline; filename="certcal.ics"'
+    return response
+
+
+if __name__ == "__main__":
+    flask_app.run(host="0.0.0.0", port=8093, debug=True)
